20b2721720fa634b553a8a490111d6db1e772b4f
Matthew Daly <matthewbdaly@gmail.com>
2015-12-26 21:10:05 +0000

    Added routing to app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Root from './components/Root';
-import Routes from './routes';
+import routes from './routes';
 import { createHistory, createMemoryHistory } from 'history';
 import { Router, RoutingContext, match } from 'react-router';
 
 export default function render(locals, callback) {
   const history = createMemoryHistory();
   const location = history.createLocation(locals.path);
+  console.log(locals);
 
-  var html = ReactDOMServer.renderToStaticMarkup(React.createElement(Root, locals));
-  callback(null, '<!DOCTYPE html>' + html);
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+    var html = ReactDOMServer.renderToStaticMarkup(<RoutingContext {...renderProps} />);
+    callback(null, '<!DOCTYPE html>' + html);
+  });
 };
