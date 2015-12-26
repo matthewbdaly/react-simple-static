669145e6de297e8399d0df40b37cc88fc3769ca3
Matthew Daly <matthewbdaly@gmail.com>
2015-12-26 20:16:24 +0000

    Now import routes file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Root from './components/Root';
+import Routes from './routes';
 
 export default function render(locals, callback) {
   var html = ReactDOMServer.renderToStaticMarkup(React.createElement(Root, locals))
