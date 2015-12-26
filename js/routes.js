69797a3cacd93a3a5b4ac0485b5508b705af8404
Matthew Daly <matthewbdaly@gmail.com>
2015-12-26 21:12:35 +0000

    Now render index component

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, DefaultRoute } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import Root from './components/Root';
 import Index from './components/Index';
 
 var Routes = (
-  <Route handler={Root} path='/'>
-    <DefaultRoute handler={Index} />
+  <Route component={Root} path='/'>
+    <IndexRoute component={Index} />
   </Route>
 )
 
