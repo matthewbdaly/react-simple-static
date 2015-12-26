1c937c704af6ecde9068d521dc27d67aef9ccf0b
Matthew Daly <matthewbdaly@gmail.com>
2015-12-26 20:18:07 +0000

    Some progress

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Root from './components/Root';
 import Routes from './routes';
+import { createHistory, createMemoryHistory } from 'history';
+import { Router, RoutingContext, match } from 'react-router';
 
 export default function render(locals, callback) {
+  const history = createMemoryHistory();
+  const location = history.createLocation(locals.path);
+
   var html = ReactDOMServer.renderToStaticMarkup(React.createElement(Root, locals))
   callback(null, '<!DOCTYPE html>' + html)
 };
